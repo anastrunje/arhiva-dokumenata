158b63ef1008989318613034aa53808c8554d215
anastrunje <astrunje@pmfst.hr>
2022-08-19 17:09:43 +0200

    Autentifikacija i login - backend

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,18 +1,19 @@
 const asyncHandler= require('express-async-handler')
 
 const Goal = require('../models/goalModel')
+const User = require('../models/userModel')
 
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
 const getGoals = asyncHandler(async (req, res) => {
-    const goals = await Goal.find()
+    const goals = await Goal.find({ user: req.user.id })
 
     res.status(200).json(goals)
 }) 
 
 // @desc    Set goal
-// @route   POST /api/goals
+// @route   POST /a pi/goals
 // @access  Private
 const setGoal =  asyncHandler(async (req, res) => {
     if(!req.body.text) {
@@ -21,8 +22,11 @@ const setGoal =  asyncHandler(async (req, res) => {
     }
 
     const goal = await Goal.create({
-        text: req.body.text
+        text: req.body.text,
+        user: req.user.id
     })
+
+
 
     res.status(200).json(goal)
 })
@@ -36,6 +40,18 @@ const updateGoal =  asyncHandler(async (req, res) => {
     if(!goal) {
         res.status(400)
         throw new Error('Wrong id')
+    }
+
+    const user = await User.findById(req.user.id)
+
+    if(!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    if(goal.user.toString() !== user.id) {
+        res.status(401)
+        throw new Error('User not authorized')
     }
 
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { new: true, })
@@ -53,8 +69,18 @@ const deleteGoal =  asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Wrong id')
     }
+ 
+    const user = await User.findById(req.user.id)
 
-    //const deletedGoal = await Goal.findByIdAndDelete(req.params.id, req.body)
+    if(!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    if(goal.user.toString() !== user.id) {
+        res.status(401)
+        throw new Error('User not authorized')
+    }
 
     await goal.remove()
 
